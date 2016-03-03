d59ddb0343a790b000a388be772ac20a85ed7344
wael <wael.bouharb@gmail.com>
2016-03-03 23:14:37 +0100

    jour 2 v1

--- server/smtp.js
+++ server/smtp.js
@@ -15,4 +15,15 @@ Meteor.startup(function () {
         sendVerificationEmail: true,
         forbidClientAccountCreation: false
     });
+
+
+
+
+
+     /*   Accounts.emailTemplates.resetPassword.text = function (user, url) {
+            url = url.replace('#/', '')
+            return " To reset your password, simply click the link below:\n\n"
+                + url;
+        };*/
+
 });
