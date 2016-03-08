1a47d81debc426cfe2462d85bfd901c3c72d543a
wael <wael.bouharb@gmail.com>
2016-03-08 15:50:34 +0100

    jour 5 v2

---
+++ client/lib/googlePlace.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wael on 08/03/2016.
+ */
+GoogleMaps.init({
+    'sensor': false, //optional
+    'key': 'AIzaSyDMpeynhXl0nsyNxzBL4aNPjQq9ekG4Za4', //optional
+    'language': 'en',  //optional
+    'libraries': 'places'
+});
